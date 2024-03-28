e4884f8c2bce63ed945f024d22e43ea567ed7871
choyonghun <43197549+choyonghun@users.noreply.github.com>
2024-03-28 16:42:08 +0900

    VO 만들기

---
+++ app/src/main/java/com/moyeo/vo/Member.java
@@ -0,0 +1,19 @@
+package com.moyeo.vo;
+
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+public class Member implements Serializable {
+
+  // 직렬화 해준다. 버전관리, 안전성, 명시성을 위해 설정해준다.
+  private static final long serialVersionUID = 100L;
+
+  private String email;
+  private String name;
+  private String password;
+  private String createdDate;
+
+
+
+}
