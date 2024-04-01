aa4453ef8573349fee43390b49e7e171ad679272
woo-be <thsckddn100@gmail.com>
2024-04-01 15:24:00 +0900

    모집 게시판 수정
    컨트롤러 수정

--- app/src/main/java/com/moyeo/controller/RecruitBoardController.java
+++ app/src/main/java/com/moyeo/controller/RecruitBoardController.java
@@ -1,5 +1,32 @@
 package com.moyeo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("recruit")
 public class RecruitBoardController {
+
+  @GetMapping("list")
+  public void list(Model model) {
+    // model.addAttribute("list",);
+  }
+
+  public void add() {
+  }
+
+  public void form() {
+  }
+
+  public void view() {
+  }
+
+  public void update() {
+  }
+
+  public void delete() {
+  }
 
 }
