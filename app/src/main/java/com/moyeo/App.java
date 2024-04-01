2f48966c1942b1edde8e0fc8b595c928e04e2a94
kimsungmo0108 <rlatjdah8585@daum.net>
2024-04-01 14:41:40 +0900

    스프링부트 적용
    컨트롤러 폴더, 프로퍼티 파일, 리소스 폴더 추가

--- app/src/main/java/com/moyeo/App.java
+++ app/src/main/java/com/moyeo/App.java
@@ -1,14 +1,25 @@
 package com.moyeo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@PropertySource({
+    "classpath:config/ncp.properties",
+    "classpath:config/ncp-secret.properties"
+})
+@Controller
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
+    }
+    @GetMapping("/home")
+    public void home() {
     }
 }
