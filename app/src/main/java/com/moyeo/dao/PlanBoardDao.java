e2b9a585eeb773f75bec81ffe2fec07988bbfd29
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-04-15 17:23:52 +0900

    후기 게시판 리스트 html style 변경 및...
    후기게시판 list.html style flex에서 grid로 변경
    계획 게시판 list구현
    websocket test 중

---
+++ app/src/main/java/com/moyeo/dao/PlanBoardDao.java
@@ -0,0 +1,12 @@
+package com.moyeo.dao;
+
+import com.moyeo.vo.PlanBoard;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PlanBoardDao {
+
+  List<PlanBoard> findAll();
+
+}
---
+++ app/src/main/java/com/moyeo/service/PlanBoardService.java
@@ -0,0 +1,10 @@
+package com.moyeo.service;
+
+import com.moyeo.vo.PlanBoard;
+import java.util.List;
+
+public interface PlanBoardService {
+
+  List<PlanBoard> list();
+
+}
