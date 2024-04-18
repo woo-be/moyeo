f941025b1c45db1268dc0d2af934ad24a17f721b
tasty13 <bodri0625@gmail.com>
2024-04-18 18:14:40 +0900

    마이페이지 모집 수락여부 확인
    - 신청자 확인 버튼 추가
    - 댓글 수정 오류 해결
    - footer 고정
    - 신청자 리스트 추가중

--- app/src/main/java/com/moyeo/controller/MypageRecruitController.java
+++ app/src/main/java/com/moyeo/controller/MypageRecruitController.java
@@ -1,8 +1,10 @@
 package com.moyeo.controller;
 
 import com.moyeo.service.RecruitBoardService;
+import com.moyeo.service.RecruitMemberService;
 import com.moyeo.vo.Member;
 import com.moyeo.vo.RecruitBoard;
+import com.moyeo.vo.RecruitMember;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +30,21 @@ public class MypageRecruitController {
       throw new Exception("로그인 하시기 바랍니다.");
     }
     model.addAttribute("mypost", recruitBoardService.mypost(loginUser.getMemberId()));
+  }
+
+  @GetMapping("posted/appl")
+  public void appllist(Model model, int recruitBoardId, HttpSession session) throws Exception {
+    Member loginUser = (Member) session.getAttribute("loginUser");
+    if (loginUser == null) {
+      throw new Exception("로그인 하시기 바랍니다.");
+    }
+
+    RecruitBoard recruitBoard = recruitBoardService.get(recruitBoardId);
+    if (!recruitBoard.getWriter().equals(loginUser)) {
+      throw new Exception("권한이 없습니다.");
+    }
+
+    model.addAttribute("applicants", recruitBoardService.findAllApplicant(recruitBoardId));
   }
 
   @GetMapping("reqpost")
--- app/src/main/java/com/moyeo/dao/RecruitBoardDao.java
+++ app/src/main/java/com/moyeo/dao/RecruitBoardDao.java
@@ -1,6 +1,7 @@
 package com.moyeo.dao;
 
 import com.moyeo.vo.RecruitBoard;
+import com.moyeo.vo.RecruitMember;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -36,6 +37,7 @@ public interface RecruitBoardDao {
       @Param("keyword")String keyword);
 
   // 조회수 증가
-
   void plusViews(int boardId);
+
+  List<RecruitMember> findAllApplicant(@Param("recruitBoardId") int recruitBoardId);
 }
