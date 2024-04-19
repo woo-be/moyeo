aa3a64e4bba1311e78a417b645c1575caa6ac39e
kimsungmo0108 <rlatjdah8585@daum.net>
2024-04-19 16:06:13 +0900

    채팅방 구현
    MessageController.java, MessageService.java, DefaultMessageService.java,
    MessageDao.java, MessageDao.xml 구현 및 수정

--- app/src/main/java/com/moyeo/controller/PlanBoardController.java
+++ app/src/main/java/com/moyeo/controller/PlanBoardController.java
@@ -1,21 +1,33 @@
 package com.moyeo.controller;
 
+import com.amazonaws.services.s3.internal.eventstreaming.Message;
 import com.moyeo.dao.PlanBoardDao;
+import com.moyeo.service.MessageService;
 import com.moyeo.service.PlanBoardService;
+import com.moyeo.vo.Member;
+import com.moyeo.vo.Msg;
 import com.moyeo.vo.PlanBoard;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/plan")
 @RequiredArgsConstructor
 @Controller
 public class PlanBoardController {
   private final PlanBoardService planBoardService;
-  private final PlanBoardDao planBoardDao;
+  private final static Log log = LogFactory.getLog(PlanBoardController.class);
 
   @GetMapping("list")
   public void list(Model model) {
@@ -29,9 +41,5 @@ public class PlanBoardController {
     model.addAttribute("planBoard", planBoardService.get(planBoardId));
  }
 
-  @GetMapping("chat")
-  public String index() {
-    return "plan/chat";
-  }
 
 }
