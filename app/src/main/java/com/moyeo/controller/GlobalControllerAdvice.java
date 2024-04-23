6562789ce49bb1e6013db770c0b7445b196f40e3
kimsungmo0108 <rlatjdah8585@daum.net>
2024-04-23 17:37:35 +0900

    알림 모달창 구현
    header.html 수정, 알림 메세지 수정

--- app/src/main/java/com/moyeo/controller/GlobalControllerAdvice.java
+++ app/src/main/java/com/moyeo/controller/GlobalControllerAdvice.java
@@ -1,9 +1,8 @@
 package com.moyeo.controller;
 
+import com.moyeo.vo.MoyeoError;
 import java.beans.PropertyEditorSupport;
 import java.sql.Date;
-import javax.servlet.http.HttpSession;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-@Log4j2
 public class GlobalControllerAdvice {
 
   @InitBinder
@@ -24,12 +22,12 @@ public class GlobalControllerAdvice {
   }
 
   @ExceptionHandler
-  public ModelAndView exceptionHandler(Exception e, HttpSession session) {
+  public ModelAndView exceptionHandler(MoyeoError e) {
     ModelAndView mv = new ModelAndView();
 
-    mv.addObject("message", session.getAttribute("message"));
-    mv.addObject("replaceUrl", session.getAttribute("replaceUrl"));
-    log.debug(e.getMessage());
+    mv.addObject("message", e.getMsg());
+    mv.addObject("replaceUrl", e.getUrl());
+
     mv.setViewName("error");
     return mv;
   }
