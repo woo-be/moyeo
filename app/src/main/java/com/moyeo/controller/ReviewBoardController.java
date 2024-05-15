c16d3e000ce65f832c1511506a81c0a6442edf78
chlorella71 <chlorella71@naver.com>
2024-05-15 15:25:21 +0900

    메인페이지 더보기 페이징처리

--- app/src/main/java/com/moyeo/controller/ReviewBoardController.java
+++ app/src/main/java/com/moyeo/controller/ReviewBoardController.java
@@ -399,7 +399,7 @@ public class ReviewBoardController {
       @RequestParam(defaultValue = "0") int themeId, // 원하는 테마 id를 요청
       Model model) {
 
-    if (pageSize < 3 || pageSize > 20) {
+    if (pageSize < 12 || pageSize > 20) {
       pageSize = 3;
     }
     if (pageNo < 1) {
@@ -408,11 +408,43 @@ public class ReviewBoardController {
 
     int numOfPage = 1;
     int numOfRecord = reviewBoardService.countAll(regionId, themeId, filter, keyword);
-    numOfPage = numOfRecord / pageSize + (numOfRecord % pageSize > 0 ? 1 : 0);
+
+    if (numOfRecord != 0) { // 해당하는 데이터가 하나라도 있다면,
+      numOfPage = numOfRecord / pageSize + (numOfRecord % pageSize > 0 ? 1 : 0);
+    }
 
     if (pageNo > numOfPage) {
       pageNo = numOfPage;
     }
+
+    /* 페이징 페이지 숫자 버튼 */
+    int[] pageButtons; // 페이징 페이지 숫자 버튼
+
+    if (numOfPage >= 5) { // a. 게시판 페이지가 5개 이상일 때,
+      pageButtons = new int[5]; // 페이지 숫자 버튼의 개수를 5개로 함.
+
+      if (pageNo <= 3) { // 1. 현재 페이지가 시작페이지에서 3페이지 이내의 페이지일 때,
+        for (int i = 0; i < 5; i++) { // 숫자 버튼이 1부터 시작하도록 함.
+          pageButtons[i] = i + 1;
+        }
+      } else if (pageNo >= (numOfPage - 2)) { // 2. 현재 페이지가 끝페이지에서 3페이지 이내의 페이지일 때,
+        int temp = numOfPage;
+        for (int i = 4; i >= 0; i--) { // 숫자 버튼이 끝페이지 -4 부터 시작하도록 함.
+          pageButtons[i] = temp--;
+        }
+      } else { // 3. 그 외의 경우,
+        int temp = pageNo - 2;
+        for (int i = 0; i < 5; i++) { // 숫자 버튼의 가운데 버튼이 현재페이지를 가리키도록 함.
+          pageButtons[i] = temp++;
+        }
+      }
+    } else { // b. 게시판 페이지가 5개 미만일 때,
+      pageButtons = new int[numOfPage]; // 페이지 숫자 버튼의 개수를 전체 페이지 개수로 함.
+      for (int i = 0; i < numOfPage; i++) { // 숫자 버튼이 1부터 시작하도록 함.
+        pageButtons[i] = i + 1;
+      }
+    }
+
 
     model.addAttribute("list", reviewBoardService.findByLikeCount(pageNo, pageSize, regionId, themeId, filter, keyword));
     model.addAttribute("filter", filter);
@@ -421,6 +453,8 @@ public class ReviewBoardController {
     model.addAttribute("pageNo", pageNo);
     model.addAttribute("pageSize", pageSize);
     model.addAttribute("numOfPage", numOfPage);
+    model.addAttribute("pageButtons", pageButtons); // 페이지 숫자 버튼
+
   }
 
   @GetMapping("listByViews")
@@ -433,7 +467,7 @@ public class ReviewBoardController {
       @RequestParam(defaultValue = "0") int themeId, // 원하는 테마 id를 요청
       Model model) {
 
-    if (pageSize < 3 || pageSize > 20) {
+    if (pageSize < 12 || pageSize > 20) {
       pageSize = 3;
     }
     if (pageNo < 1) {
@@ -442,10 +476,41 @@ public class ReviewBoardController {
 
     int numOfPage = 1;
     int numOfRecord = reviewBoardService.countAll(regionId, themeId, filter, keyword);
-    numOfPage = numOfRecord / pageSize + (numOfRecord % pageSize > 0 ? 1 : 0);
+
+    if (numOfRecord != 0) { // 해당하는 데이터가 하나라도 있다면,
+      numOfPage = numOfRecord / pageSize + (numOfRecord % pageSize > 0 ? 1 : 0);
+    }
 
     if (pageNo > numOfPage) {
       pageNo = numOfPage;
+    }
+
+    /* 페이징 페이지 숫자 버튼 */
+    int[] pageButtons; // 페이징 페이지 숫자 버튼
+
+    if (numOfPage >= 5) { // a. 게시판 페이지가 5개 이상일 때,
+      pageButtons = new int[5]; // 페이지 숫자 버튼의 개수를 5개로 함.
+
+      if (pageNo <= 3) { // 1. 현재 페이지가 시작페이지에서 3페이지 이내의 페이지일 때,
+        for (int i = 0; i < 5; i++) { // 숫자 버튼이 1부터 시작하도록 함.
+          pageButtons[i] = i + 1;
+        }
+      } else if (pageNo >= (numOfPage - 2)) { // 2. 현재 페이지가 끝페이지에서 3페이지 이내의 페이지일 때,
+        int temp = numOfPage;
+        for (int i = 4; i >= 0; i--) { // 숫자 버튼이 끝페이지 -4 부터 시작하도록 함.
+          pageButtons[i] = temp--;
+        }
+      } else { // 3. 그 외의 경우,
+        int temp = pageNo - 2;
+        for (int i = 0; i < 5; i++) { // 숫자 버튼의 가운데 버튼이 현재페이지를 가리키도록 함.
+          pageButtons[i] = temp++;
+        }
+      }
+    } else { // b. 게시판 페이지가 5개 미만일 때,
+      pageButtons = new int[numOfPage]; // 페이지 숫자 버튼의 개수를 전체 페이지 개수로 함.
+      for (int i = 0; i < numOfPage; i++) { // 숫자 버튼이 1부터 시작하도록 함.
+        pageButtons[i] = i + 1;
+      }
     }
 
     model.addAttribute("list", reviewBoardService.findByViews(pageNo, pageSize, regionId, themeId, filter, keyword));
@@ -455,6 +520,8 @@ public class ReviewBoardController {
     model.addAttribute("pageNo", pageNo);
     model.addAttribute("pageSize", pageSize);
     model.addAttribute("numOfPage", numOfPage);
+    model.addAttribute("pageButtons", pageButtons); // 페이지 숫자 버튼
+
   }
 
 }
